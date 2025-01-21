64c09fd1885628d0c9ed234605f0badc0899b38d
nkuek <nkuek1@gmail.com>
2025-01-21 18:18:34 -0500

    add padding

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,8 +9,8 @@ const hideLeva = true;
 export default function Home() {
   return (
     <div className="h-svh grid place-items-center w-full">
-      <div className="grid place-items-center h-[70%] w-[60%] mix-blend-difference bg-white text-slate-800 z-[1] max-w-[1000px]">
-        <ul className="flex flex-col gap-8 justify-center h-full">
+      <div className="grid place-items-center h-[70%] w-[75%] mix-blend-difference bg-white text-slate-800 z-[1] max-w-[1000px]">
+        <ul className="flex flex-col gap-8 justify-center h-full p-4">
           {Object.entries(sitemap)
             .filter(([key]) => key !== "home")
             .map(([, link]) => (
