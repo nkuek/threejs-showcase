1b4878d2bee5f95622135206f78a54e779132f21
nkuek <nkuek1@gmail.com>
2025-01-20 20:19:06 -0500

    add light movement

--- src/pages/LightShading/index.tsx
+++ src/pages/LightShading/index.tsx
@@ -66,167 +66,200 @@ function CanvasContent() {
   const torusRef = useRef<THREE.Mesh>(null);
   const suzanneRef = useRef<THREE.Group>(null);
   const sphereRef = useRef<THREE.Mesh>(null);
+  const pointLight1Ref = useRef<THREE.Mesh>(null);
+  const pointLight2Ref = useRef<THREE.Mesh>(null);
+  const shaderMaterialRef = useRef<THREE.ShaderMaterial>(null);
 
-  const { ambientLightColor, ambientLightIntensity, ambientLightEnabled } =
-    useControls({
-      "Ambient Light": folder({
-        ambientLightColor: {
-          value: "#ffffff",
-          label: "Color",
-        },
-        ambientLightIntensity: {
-          value: 0.03,
-          min: 0,
-          max: 0.1,
-          step: 0.01,
-          label: "Intensity",
-        },
-        ambientLightEnabled: {
-          value: true,
-          label: "Enabled",
-        },
-      }),
-    });
-
-  const {
-    directionalLightPosition,
-    directionalLightColor,
-    directionalLightSpecularPower,
-    directionalLightIntensity,
-    directionalLightEnabled,
-  } = useControls({
-    "Directional Light": folder({
-      directionalLightPosition: {
-        value: [0, 0, 3],
-        step: 0.1,
-        label: "Position",
-      },
-      directionalLightColor: {
-        value: "#3b82ff",
-        label: "Color",
-      },
-      directionalLightSpecularPower: {
-        value: 20,
-        min: 1,
-        max: 100,
-        step: 0.1,
-        label: "Specular Power",
-      },
-      directionalLightIntensity: {
-        value: 1,
-        min: 0,
-        max: 10,
-        step: 0.1,
-        label: "Intensity",
-      },
-      directionalLightEnabled: {
-        value: true,
-        label: "Enabled",
-      },
-    }),
-  });
-
-  const {
-    pointLight1Position,
-    pointLight1Color,
-    pointLight1SpecularPower,
-    pointLight1Intensity,
-    pointLight1Decay,
-    pointLight1Enabled,
-  } = useControls({
-    "Point Light 1": folder({
-      pointLight1Position: {
-        value: [0, 2.5, 0],
-        step: 0.1,
-        label: "Position",
-      },
-      pointLight1Color: {
-        value: "#ff6060",
-        label: "Color",
-      },
-      pointLight1SpecularPower: {
-        value: 20,
-        min: 1,
-        max: 100,
-        step: 0.1,
-        label: "Specular Power",
-      },
-      pointLight1Intensity: {
-        value: 1,
-        min: 0,
-        max: 10,
-        step: 0.1,
-        label: "Intensity",
-      },
-      pointLight1Decay: {
-        value: 0.25,
-        min: 0,
-        max: 1,
-        step: 0.01,
-        label: "Decay",
-      },
-      pointLight1Enabled: {
-        value: true,
-        label: "Enabled",
-      },
-    }),
-  });
-
-  const { modelColor } = useControls({
+  const { modelColor, moveLights } = useControls({
     modelColor: {
       value: "#ffffff",
       label: "Model Color",
     },
+    moveLights: {
+      value: true,
+      label: "Move Lights",
+    },
   });
+  const { ambientLightColor, ambientLightIntensity, ambientLightEnabled } =
+    useControls({
+      "Ambient Light": folder(
+        {
+          ambientLightColor: {
+            value: "#ffffff",
+            label: "Color",
+          },
+          ambientLightIntensity: {
+            value: 0.03,
+            min: 0,
+            max: 0.1,
+            step: 0.01,
+            label: "Intensity",
+          },
+          ambientLightEnabled: {
+            value: true,
+            label: "Enabled",
+          },
+        },
+        { collapsed: true }
+      ),
+    });
 
-  const {
-    pointLight2Position,
-    pointLight2Color,
-    pointLight2SpecularPower,
-    pointLight2Intensity,
-    pointLight2Decay,
-    pointLight2Enabled,
-  } = useControls({
-    "Point Light 2": folder({
-      pointLight2Position: {
-        value: [2, 2, 2],
-        step: 0.1,
-        label: "Position",
+  const [
+    {
+      directionalLightPosition,
+      directionalLightColor,
+      directionalLightSpecularPower,
+      directionalLightIntensity,
+      directionalLightEnabled,
+    },
+    setDirectionalLight,
+  ] = useControls(() => ({
+    "Directional Light": folder(
+      {
+        directionalLightPosition: {
+          value: [0, 0, 3],
+          step: 0.1,
+          label: "Position",
+        },
+        directionalLightColor: {
+          value: "#3b82ff",
+          label: "Color",
+        },
+        directionalLightSpecularPower: {
+          value: 20,
+          min: 1,
+          max: 100,
+          step: 0.1,
+          label: "Specular Power",
+        },
+        directionalLightIntensity: {
+          value: 1,
+          min: 0,
+          max: 10,
+          step: 0.1,
+          label: "Intensity",
+        },
+        directionalLightEnabled: {
+          value: true,
+          label: "Enabled",
+        },
       },
-      pointLight2Color: {
-        value: "#61ff9a",
-        label: "Color",
+      { collapsed: true }
+    ),
+  }));
+
+  const [
+    {
+      pointLight1Position,
+      pointLight1Color,
+      pointLight1SpecularPower,
+      pointLight1Intensity,
+      pointLight1Decay,
+      pointLight1Enabled,
+    },
+    setPointLight1,
+  ] = useControls(() => ({
+    "Point Light 1": folder(
+      {
+        pointLight1Position: {
+          value: [0, 2.5, 0],
+          step: 0.1,
+          label: "Position",
+        },
+        pointLight1Color: {
+          value: "#ff6060",
+          label: "Color",
+        },
+        pointLight1SpecularPower: {
+          value: 20,
+          min: 1,
+          max: 100,
+          step: 0.1,
+          label: "Specular Power",
+        },
+        pointLight1Intensity: {
+          value: 1,
+          min: 0,
+          max: 10,
+          step: 0.1,
+          label: "Intensity",
+        },
+        pointLight1Decay: {
+          value: 0.25,
+          min: 0,
+          max: 1,
+          step: 0.01,
+          label: "Decay",
+        },
+        pointLight1Enabled: {
+          value: true,
+          label: "Enabled",
+        },
       },
-      pointLight2SpecularPower: {
-        value: 20,
-        min: 1,
-        max: 100,
-        step: 0.1,
-        label: "Specular Power",
+      { collapsed: true }
+    ),
+  }));
+
+  const [
+    {
+      pointLight2Position,
+      pointLight2Color,
+      pointLight2SpecularPower,
+      pointLight2Intensity,
+      pointLight2Decay,
+      pointLight2Enabled,
+    },
+    setPointLight2,
+  ] = useControls(() => ({
+    "Point Light 2": folder(
+      {
+        pointLight2Position: {
+          value: [0, 2, 2],
+          step: 0.1,
+          label: "Position",
+        },
+        pointLight2Color: {
+          value: "#61ff9a",
+          label: "Color",
+        },
+        pointLight2SpecularPower: {
+          value: 20,
+          min: 1,
+          max: 100,
+          step: 0.1,
+          label: "Specular Power",
+        },
+        pointLight2Intensity: {
+          value: 1,
+          min: 0,
+          max: 10,
+          step: 0.1,
+          label: "Intensity",
+        },
+        pointLight2Decay: {
+          value: 0.2,
+          min: 0,
+          max: 1,
+          step: 0.01,
+          label: "Decay",
+        },
+        pointLight2Enabled: {
+          value: true,
+          label: "Enabled",
+        },
       },
-      pointLight2Intensity: {
-        value: 1,
-        min: 0,
-        max: 10,
-        step: 0.1,
-        label: "Intensity",
-      },
-      pointLight2Decay: {
-        value: 0.2,
-        min: 0,
-        max: 1,
-        step: 0.01,
-        label: "Decay",
-      },
-      pointLight2Enabled: {
-        value: true,
-        label: "Enabled",
-      },
-    }),
-  });
+      { collapsed: true }
+    ),
+  }));
 
   useFrame(({ clock }) => {
-    if (!torusRef.current || !suzanneRef.current || !sphereRef.current) return;
+    if (
+      !torusRef.current ||
+      !suzanneRef.current ||
+      !sphereRef.current ||
+      !shaderMaterialRef.current
+    )
+      return;
     const elapsedTime = clock.getElapsedTime();
 
     torusRef.current.rotation.x = elapsedTime * 0.1;
@@ -237,11 +270,23 @@ function CanvasContent() {
 
     sphereRef.current.rotation.x = elapsedTime * 0.1;
     sphereRef.current.rotation.y = elapsedTime * 0.2;
+
+    if (!moveLights) return;
+
+    const light1PositionX = Math.sin(elapsedTime) * 4;
+    const light2PositionX = Math.sin(elapsedTime) * 4;
+    const directionalLightPositionY = Math.cos(elapsedTime * 1.2) * 3;
+    setPointLight1({ pointLight1Position: [light1PositionX, 2.5, 0] });
+    setPointLight2({ pointLight2Position: [light2PositionX, 2, 2] });
+    setDirectionalLight({
+      directionalLightPosition: [0, directionalLightPositionY, 3],
+    });
   });
 
   function ShaderMaterial() {
     return (
       <customShaderMaterial
+        ref={shaderMaterialRef}
         // NOTE: add key to allow HMR of shader
         key={CustomShaderMaterial.key}
         uColor={modelColor}
@@ -293,11 +338,19 @@ function CanvasContent() {
           color={directionalLightColor}
         />
       </mesh>
-      <mesh position={pointLight1Position} visible={pointLight1Enabled}>
+      <mesh
+        position={pointLight1Position}
+        visible={pointLight1Enabled}
+        ref={pointLight1Ref}
+      >
         <icosahedronGeometry args={[0.1, 2]} />
         <meshBasicMaterial color={pointLight1Color} />
       </mesh>
-      <mesh position={pointLight2Position} visible={pointLight2Enabled}>
+      <mesh
+        position={pointLight2Position}
+        visible={pointLight2Enabled}
+        ref={pointLight2Ref}
+      >
         <icosahedronGeometry args={[0.1, 2]} />
         <meshBasicMaterial color={pointLight2Color} />
       </mesh>
@@ -310,8 +363,8 @@ function CanvasContent() {
 export default function LightShading() {
   return (
     <div className="h-svh bg-slate-800 w-full relative">
-      <div className="absolute w-[400px] right-0 z-10">
-        <Leva fill />
+      <div className="absolute w-[400px] right-0 top-[76px] z-10">
+        <Leva fill collapsed />
       </div>
       <Canvas>
         <CanvasContent />
