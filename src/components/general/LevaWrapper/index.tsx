cc97723a2f59331de2f61b53492a54d0619d7a95
nkuek <nkuek1@gmail.com>
2025-01-23 00:21:16 -0500

    improve leva positioning

--- src/components/general/LevaWrapper/index.tsx
+++ src/components/general/LevaWrapper/index.tsx
@@ -2,7 +2,7 @@ import { Leva } from "leva";
 
 export default function LevaWrapper() {
   return (
-    <div className="absolute w-[400px] right-0 top-[76px] z-10">
+    <div className="absolute w-[400px] right-0 top-28 z-10">
       <Leva fill collapsed />
     </div>
   );
