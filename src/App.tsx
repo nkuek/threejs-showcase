4f75599fdcbdb6ce9b2201571a09d59ddb961525
nkuek <nkuek1@gmail.com>
2025-05-21 18:48:31 -0400

    use regular browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import AppContextProvider from "~/components/general/AppProvider";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import Router from "~/components/general/Router";
 
 function App() {
   return (
     <div className="bg-stone-100">
       <AppContextProvider>
-        <HashRouter>
+        <BrowserRouter>
           <Router />
-        </HashRouter>
+        </BrowserRouter>
       </AppContextProvider>
     </div>
   );
