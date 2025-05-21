b7df7ddf05eea731cedae0de0ee9d35466454c38
nkuek <nkuek1@gmail.com>
2025-05-21 19:00:29 -0400

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AppContextProvider from "~/components/general/AppProvider";
 import { BrowserRouter } from "react-router";
 import Router from "~/components/general/Router";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <div className="bg-stone-100">
+      <Analytics />
       <AppContextProvider>
         <BrowserRouter>
           <Router />
